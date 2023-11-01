982bb31118dfa78ef366d7835a2c56698df85c76
NanobotArtem2208 <106172615+NanobotArtem2208@users.noreply.github.com>
2023-11-01 17:32:51 +0400

    Update ProductItem.jsx

--- src/components/ProductItem/ProductItem.jsx
+++ src/components/ProductItem/ProductItem.jsx
@@ -10,13 +10,13 @@ const ProductItem = ({product, className, onAdd}) => {
 
     return (
         <div className={'product ' + className}>
-            <div className={'img'}/>
-            <div className={'title'}>{product.title}</div>
-            <div className={'description'}>{product.description}</div>
-            <div className={'price'}>
+            <div className='imgContainer'><img className='img' src={product.img} alt={product.title} /></div>
+            <div className='title'>{product.title}</div>
+            <div className='description'>{product.description}</div>
+            <div className='price'>
                 <span>Стоимость: <b>{product.price}</b></span>
             </div>
-            <Button className={'add-btn'} onClick={onAddHandler}>
+            <Button className='add-btn' onClick={onAddHandler}>
                 Добавить в корзину
             </Button>
         </div>
